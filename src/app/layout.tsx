d626c712fd28d39a00319cc33a4f37ce2305baa8
zouno23 <medazizbenhmidene@gmail.com>
2024-11-03 14:55:37 +0100

    added auth and db connection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,10 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
@@ -13,8 +20,10 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
+    <ClerkProvider>
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
